0c2a140fb0127511327238690829fb001dd32401
Dartadaar <dartadaar@gmail.com>
2024-07-24 18:29:49 +0200

    mobile list of works added

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -3,12 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:wzukowski_com/constants/colors.dart';
 import 'package:wzukowski_com/constants/size.dart';
 import 'package:wzukowski_com/constants/sns_links.dart';
-import 'package:wzukowski_com/constants/works.dart';
 import 'package:wzukowski_com/widgets/custom_text_field.dart';
 import 'package:wzukowski_com/widgets/drawer_mobile.dart';
 import 'package:wzukowski_com/widgets/header_desktop.dart';
 import 'package:wzukowski_com/widgets/header_mobile.dart';
-import 'package:wzukowski_com/widgets/separatorline.dart';
+import 'package:wzukowski_com/widgets/list_of_works_desktop.dart';
+import 'package:wzukowski_com/widgets/list_of_works_mobile.dart';
 import 'package:wzukowski_com/widgets/site_logo.dart';
 // ignore: avoid_web_libraries_in_flutter
 import 'dart:js' as js;
@@ -162,76 +162,7 @@ class _HomepageState extends State<Homepage> {
                   key: navbarKeys[1],
                   width: double.maxFinite,
                   color: CustomColor.blackPrimary,
-                  child:
-                  Column(
-                    children: [
-                      const SizedBox(
-                        width: double.maxFinite,
-                        height: 30,
-                      ),
-                      for(int i=0; i<workTypes.length; i++) //pętla rodzajów
-                      Column(
-                        children: [
-                          Row(
-                            mainAxisSize: MainAxisSize.max,
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Flexible(
-                                flex: 1,
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Container(
-                                      padding: const EdgeInsets.all(20),
-                                      //color: Colors.green,
-                                      //width: constraints.maxWidth/2,
-                                      child: Text(workTypes[i], style: const TextStyle(fontSize: 24),),
-                                    )
-                                  ]
-                                ),
-                              ),
-                              Flexible(
-                                flex: 1,
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.end, children:[
-                                    for (int ii=0;ii<worksList.length;ii++)
-                                    if (worksList[ii][3]==i)
-                                    Container(
-                                      //width: constraints.maxWidth/2,
-                                      padding: const EdgeInsets.all(10),
-                                      child: Column(
-                                        children: [
-                                          Container(
-                                            padding: const EdgeInsets.only(top: 20, bottom: 40),
-                                            child: Row(
-                                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                              crossAxisAlignment: CrossAxisAlignment.start,
-                                              children: [
-                                                Expanded(
-                                                    child:
-                                                    worksList[ii].length >= 5 ? InkWell(onTap: (){js.context.callMethod('open', [worksList[ii][4]]);}, child: Text('${worksList[ii][0]} for ${worksList[ii][1]}', maxLines: 4, overflow: TextOverflow.ellipsis, textDirection: TextDirection.rtl, textAlign: TextAlign.left, style: const TextStyle(fontSize: 18, decoration: TextDecoration.underline),)) : Text('${worksList[ii][0]} for ${worksList[ii][1]}', maxLines: 4, overflow: TextOverflow.ellipsis, textDirection: TextDirection.rtl, textAlign: TextAlign.left, style: const TextStyle(fontSize: 18),)
-                                                ),
-                                                // ignore: unnecessary_null_comparison
-                                                Container(padding: const EdgeInsets.only(left: 20),child: Text(worksList[ii][2] == null ? "Empty" : '${worksList[ii][2]}', style: const TextStyle(fontSize: 18),)) //rok
-                                              ]
-                                             ),
-                                          ),
-                                           const Divider(color: CustomColor.whitePrimary, height: 0,)
-                                        ],
-                                      ),
-                                    )
-                                    ]
-                                ),
-                              ),
-                            ]
-                          ),
-                          if (i<workTypes.length-1)
-                          const SeparatorLine()
-                        ],
-                      ),
-                    ],
-                  )
+                  child: constraints.maxWidth >= kMinDesktopWidth ? const ListOfWorksDesktop() : const ListOfWorksMobile()
                 ),
                 const Padding(
                   padding: EdgeInsets.symmetric(vertical: 25),
---
+++ lib/widgets/list_of_works_desktop.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+// ignore: avoid_web_libraries_in_flutter
+import 'dart:js' as js;
+
+import '../constants/colors.dart';
+import '../constants/works.dart';
+import 'separatorline.dart';
+
+class ListOfWorksDesktop extends StatelessWidget {
+  const ListOfWorksDesktop({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const SizedBox(
+          width: double.maxFinite,
+          height: 30,
+        ),
+        for (int i = 0; i < workTypes.length; i++) //pętla rodzajów
+          Column(
+            children: [
+              Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Flexible(
+                      flex: 1,
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              padding: const EdgeInsets.all(20),
+                              //color: Colors.green,
+                              //width: constraints.maxWidth/2,
+                              child: Text(
+                                workTypes[i],
+                                style: const TextStyle(fontSize: 24),
+                              ),
+                            )
+                          ]),
+                    ),
+                    Flexible(
+                      flex: 1,
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            for (int ii = 0; ii < worksList.length; ii++)
+                              if (worksList[ii][3] == i)
+                                Container(
+                                  //width: constraints.maxWidth/2,
+                                  padding: const EdgeInsets.all(10),
+                                  child: Column(
+                                    children: [
+                                      Container(
+                                        padding: const EdgeInsets.only(
+                                            top: 20, bottom: 40),
+                                        child: Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.spaceBetween,
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              Expanded(
+                                                  child: worksList[ii].length >=
+                                                          5
+                                                      ? InkWell(
+                                                          onTap: () {
+                                                            js.context
+                                                                .callMethod(
+                                                                    'open', [
+                                                              worksList[ii][4]
+                                                            ]);
+                                                          },
+                                                          child: Text(
+                                                            '${worksList[ii][0]} for ${worksList[ii][1]}',
+                                                            maxLines: 4,
+                                                            overflow:
+                                                                TextOverflow
+                                                                    .ellipsis,
+                                                            textDirection:
+                                                                TextDirection
+                                                                    .rtl,
+                                                            textAlign:
+                                                                TextAlign.left,
+                                                            style: const TextStyle(
+                                                                fontSize: 18,
+                                                                decoration:
+                                                                    TextDecoration
+                                                                        .underline),
+                                                          ))
+                                                      : Text(
+                                                          '${worksList[ii][0]} for ${worksList[ii][1]}',
+                                                          maxLines: 4,
+                                                          overflow: TextOverflow
+                                                              .ellipsis,
+                                                          textDirection:
+                                                              TextDirection.rtl,
+                                                          textAlign:
+                                                              TextAlign.left,
+                                                          style:
+                                                              const TextStyle(
+                                                                  fontSize: 18),
+                                                        )),
+                                              // ignore: unnecessary_null_comparison
+                                              Container(
+                                                  padding:
+                                                      const EdgeInsets.only(
+                                                          left: 20),
+                                                  child: Text(
+                                                    worksList[ii][2] == 0
+                                                        ? "Empty"
+                                                        : '${worksList[ii][2]}',
+                                                    style: const TextStyle(
+                                                        fontSize: 18),
+                                                  )) //rok
+                                            ]),
+                                      ),
+                                      const Divider(
+                                        color: CustomColor.whitePrimary,
+                                        height: 0,
+                                      )
+                                    ],
+                                  ),
+                                )
+                          ]),
+                    ),
+                  ]),
+              if (i < workTypes.length - 1) const Padding(
+                padding: EdgeInsets.only(top: 30),
+                child: SeparatorLine(),
+              )
+            ],
+          ),
+      ],
+    );
+  }
+}
