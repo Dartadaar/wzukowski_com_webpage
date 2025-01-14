4bae6c6f59059a451d16bcd482dc69b0aa0b306f
Dartadaar <dartadaar@gmail.com>
2025-01-14 09:43:12 +0100

    works upd

--- lib/constants/works.dart
+++ lib/constants/works.dart
@@ -1,12 +1,12 @@
 var worksList = [
   ["endsay II", "va, small ensemble (fl, cl, acc, vn, vc, cb) and media", 2025, 1],
-  ["endsay", "va, 2.0 playback and media", 2024, 1],
+  ["endsay I", "va, 2.0 playback and media", 2024, 1],
   ["nomads II", "vn and cbas clarinet", 2024, 0],
   ["Cool black / Bellflower color (Kikyō-iro)", "small ensemble (picc, cl, perc, acc, gtr, vn, va, vc)", 2024, 0],
   ["offset", "any melodic instrument/combination of instruments", 2023, 0],
   ["Pyramid texts 436", "5 voices and small ensemble", 2023, 0, "https://www.youtube.com/watch?v=9uDACnbZRQs"],
   ["Innermost", "10 string instruments and piano", 2022, 0],
-  ["Concerto", "quarter-tone hammond organ, harp and string orchestra", 2022, 0, "https://soundcloud.com/dartadaar/koncert-v2?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"],
+  ["Concerto", "quarter-tone hammond organ, harp and string orchestra", 2022, 0, "https://soundcloud.com/wzukowski/koncert-v2?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"],
   ["Book of Coming Forth by Day", "bass clarinet and harp", 2021, 0, "https://youtu.be/dY7EPQdPPA4?si=uPpJ9g2AM_5TQEqT"],
   ["Consecutio temporum", "tenor saxophone and percussion", 2020, 0, "https://www.youtube.com/watch?v=X1KQVqNx5to"],
   ["an orange balloon drifting over snow-capped mountains", "electric guitar, ensemble (fl, ob, bcl, cbn, hn, 2acc, hp, 2vn, 2va), live electronics and lights", 2023, 1],
@@ -15,7 +15,7 @@ var worksList = [
   ["IT IS NO NIGHT TO DROWN IN", "female voice, wind quartet (ssax, 2cl, bcl), live electronics and video", 2023, 1],
   ["Dispute between a man and his Ba", "baritone, harp, live electronics and lights", 2022, 1, "https://youtu.be/FoOrYkL__nY?si=xwkrbkgCZZMuZcaw"],
   ["ashling scattered over several provinces (Dolby Atmos)", "dolby atmos playback", 2024, 2],
-  ["postludium", "dolby atmos playback", 2023, 2, "https://soundcloud.com/dartadaar/postludium-binaural-headphones-recommended?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"],
+  ["postludium", "dolby atmos playback", 2023, 2, "https://soundcloud.com/wzukowski/postludium-binaural-headphones-recommended?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"],
   ["Etüden der elektronischen Musik", "2.0 playback", 2022, 2],
   ["luna", "2.0 playback", 2022, 2, "https://recordu.lnk.to/4W88xn"]
 ];
